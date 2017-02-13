b7ad9eecdabfef147a388e87c2e6f2ae4dfa79b7
ToF <cthibaut@octo.com>
2017-02-13 15:12:17 +0100

    joining two areas

--- Kami.hs
+++ Kami.hs
@@ -9,3 +9,18 @@ data Color = Brown | Red
 
 success :: GameState -> Bool
 success = (1==) . length
+
+color = fst
+squares = snd
+
+join :: Area -> Area -> Area
+join a b | (color a == color b) && any (neighbor (squares b)) (squares a) = (color a,squares a ++ squares b)
+         | otherwise = a
+    where
+    neighbor :: [Coords] -> Coords -> Bool
+    neighbor cs c = any (touches c) cs
+        where
+        touches (x,y) (x',y') = dx*dy == 0 && dx+dy == 1
+            where
+            dx = abs (x-x')
+            dy = abs (y-y')
--- Specs.hs
+++ Specs.hs
@@ -11,4 +11,14 @@ main = hspec $ do
                             ,(2,0),(3,0),(2,1),(3,1)])]
                 success first `shouldBe` False
                 success second `shouldBe` True
+    describe "area" $ do
+        it "can join another area if they are neighbors and same color" $ do
+            let a = (Brown,[(0,0),(1,0)])
+                b = (Red,  [(0,1),(0,2)])
+                c = (Brown,[(1,1),(1,2)])
+                d = (Brown,[(0,2),(2,2)])
+            join a b `shouldBe` a 
+            join a c `shouldBe` (Brown,[(0,0),(1,0),(1,1),(1,2)])
+            join a d `shouldBe` a
+                 
                 
