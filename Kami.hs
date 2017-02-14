f2220374330d96b7fecb434cdfb06b9f79276127
ToF <cthibaut@octo.com>
2017-02-14 17:37:16 +0100

    game: parse text and joins areas

--- Kami.hs
+++ Kami.hs
@@ -3,14 +3,52 @@ where
 import Data.List
 
 type GameState = [Area]
-type Area = (Color,[Coord])
-type Coord = (Int,Int)
+type Area = [(Coord,Color)]
+type Coord = (Integer,Integer)
 data Color = Brown | Blue | Cyan | Orange | Green | Red | White | Yellow 
     deriving (Eq,Ord,Show)
 
 game :: [String] -> GameState
-game ss = map zip [0..] ss
+game = sort . map sort . join . singletons . mapGame
+    where
+    index :: [a] -> [(Integer,a)]
+    index = zip [0..]
 
-success :: GameState -> Bool 
-success _ = True
+    coordx :: Integer -> [a] -> [(Coord,a)]
+    coordx y s = [((x,y),c)  | (x,c) <- index s]
+
+    coordy :: [[a]] -> [[(Coord,a)]]
+    coordy  ss = [coordx y s | (y,s) <- index ss]
+    
+    singletons :: [[a]] -> [[a]]
+    singletons = concatMap (map return)
+
+    mapGame :: [[Char]] -> GameState
+    mapGame = singletons . coordy . map (map charToColor)
+
+charToColor :: Char -> Color
+charToColor '#' = Brown
+charToColor '.' = Blue
+charToColor '*' = Orange
+charToColor '@' = Red
+
+color = snd . head
+squares = map fst
+
+touch :: Area -> Area -> Bool
+touch a b | color a /= color b = False
+touch a b = any (\cd -> any (\cd' -> cd `near` cd') (squares b)) (squares a)  
+    where 
+    near (x,y) (x',y') = dx * dy == 0 && dx + dy == 1
+        where 
+        dx = abs (x - x')
+        dy = abs (y - y')
+
+join :: GameState -> GameState
+join []  = []
+join [a] = [a]
+join (a:b:as) | touch a b = join ((a++b):join as)
+              | otherwise = b:join (a:join as)
+
+
 
--- Specs.hs
+++ Specs.hs
@@ -5,6 +5,24 @@ import Kami
 main = hspec $ do
     describe "game" $ do
         it "defines a game state by reading text" $ do
-            success (game ["##"]) `shouldBe` True
-            success (game ["#."]) `shouldBe` False
+            game [] `shouldBe`  []
+            game ["#"] `shouldBe` [[((0,0),Brown)]]
+            game ["."] `shouldBe` [[((0,0),Blue)]]
+            game [".#"] `shouldBe`[[((0,0),Blue)],[((1,0),Brown)]]
+            game [".#","@*"] `shouldBe`[[((0,0),Blue)],[((0,1),Red)],[((1,0),Brown)],[((1,1),Orange)]]
+
+        it "joins group of contiguous cells of same color" $ do
+            game [".."] `shouldBe` [[((0,0),Blue),((1,0),Blue)]]
+            game ["##."
+                 ,"#.."
+                 ,"..@"] `shouldBe` [[((0,0),Brown),((0,1),Brown),((1,0),Brown)]
+                                    ,[((0,2),Blue),((1,1),Blue),((1,2),Blue),((2,0),Blue),((2,1),Blue)]
+                                    ,[((2,2),Red)]]
+            game ["##."
+                 ,"#@."
+                 ,"..@"] `shouldBe` [[((0,0),Brown),((0,1),Brown),((1,0),Brown)]
+                                    ,[((0,2),Blue),((1,2),Blue)]
+                                    ,[((1,1),Red)]
+                                    ,[((2,0),Blue),((2,1),Blue)]
+                                    ,[((2,2),Red)]]
 
