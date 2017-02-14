22cf19dce542ff1cad3953585158b49c4215c339
ToF <cthibaut@octo.com>
2017-02-14 13:27:26 +0100

    restart

--- Kami.hs
+++ Kami.hs
@@ -3,47 +3,14 @@ where
 import Data.List
 
 type GameState = [Area]
-type Area = (Color,[Coords])
-type Coords = (Int,Int)
-data Color = Brown | Red | Blue
-    deriving (Eq, Ord, Show)
-type Move = (Color, Coords)
+type Area = (Color,[Coord])
+type Coord = (Int,Int)
+data Color = Brown | Blue | Cyan | Orange | Green | Red | White | Yellow 
+    deriving (Eq,Ord,Show)
 
-success :: GameState -> Bool
-success = (1==) . length
+game :: [String] -> GameState
+game ss = map zip [0..] ss
 
-touch :: Area -> Area -> Bool
-touch a b | color a /= color b = False
-touch a b = any (\cd -> any (\cd' -> cd `near` cd') (squares b)) (squares a)  
-    where 
-    near (x,y) (x',y') = dx * dy == 0 && dx + dy == 1
-        where 
-        dx = abs (x - x')
-        dy = abs (y - y')
-
-color = fst
-squares = snd
-
-join :: GameState -> GameState
-join []  = []
-join [a] = [a]
-join (a:b:as) | touch a b = join ((color a, squares a ++ squares b):join as)
-              | otherwise = b:join (a:join as)
-
-change :: Move -> GameState -> GameState
-change _ [] = []
-change (c,(x,y)) ((c',sq):as) | (x,y) `elem` sq = (c,sq):as
-                            | otherwise       = (c',sq):change (c,(x,y)) as
-
-moves :: GameState -> [Move]
-moves g = concatMap (\(c,sq) -> [(c',head sq) | c' <- colors g, c' /= c]) g
-    where colors = nub . sort . map color 
-
-solve :: Int -> GameState -> [(Int,Move)]
-solve n = solve' n 0 
-    where 
-    solve' 0 _ _ = []
-    solve' 1 n g = [[(n,m)] | m <- moves g, success (join (change m g))]
-    solve' 2 n g = [(n,m) | m <- moves g,  
-
+success :: GameState -> Bool 
+success _ = True
 
--- Specs.hs
+++ Specs.hs
@@ -2,57 +2,9 @@ import Test.Hspec
 import Data.List (sort)
 import Kami
 
-a = (Brown,[(0,0),(1,0)])
-b = (Red,  [(0,1),(0,2)])
-c = (Brown,[(1,1),(1,2)])
-d = (Brown,[(0,2),(1,2)])
-
-gsSort = sort . map (\(c,sq)-> (c,sort sq))
-
 main = hspec $ do
-    describe "touch" $ do
-        it "should be true if an area touches another one of same color" $ do
-            (a `touch` b) `shouldBe` False
-            (a `touch` c) `shouldBe` True
-            (a `touch` d) `shouldBe` False
+    describe "game" $ do
+        it "defines a game state by reading text" $ do
+            success (game ["##"]) `shouldBe` True
+            success (game ["#."]) `shouldBe` False
 
-    describe "game state" $ do
-        describe "is made of area that are groups of squares of same color" $ do
-            it "is a success when there's only one group" $ do
-                let first = [(Brown,[(0,0),(1,0),(0,1),(1,1)])
-                            ,(Red,  [(2,0),(3,0),(2,1),(3,1)])]
-                    second= [(Brown,[(0,0),(1,0),(0,1),(1,1)
-                            ,(2,0),(3,0),(2,1),(3,1)])]
-                success first `shouldBe` False
-                success second `shouldBe` True
-    
-    describe "merge" $ do
-        it "join areas that touch together and are of same color" $ do
-            let a' = (Brown,[(0,0),(1,0),(1,1),(1,2),(0,2),(1,2)])
-            gsSort (join [a,b,d,c]) `shouldBe` (gsSort [a',b])
-    
-            (gsSort (join [a,b])) `shouldBe` (gsSort [a,b])
-
-    describe "change" $ do
-        it "changes the color of an area" $ do
-            let g = join (change (Brown,(0,1)) [a,b,c,d]) 
-            (gsSort g) `shouldBe` (gsSort [(Brown,[(0,0),(1,0),(0,1),(0,2),(1,1),(1,2),(0,2),(1,2)])])
-
-    describe "moves" $ do
-        it "gives all the possible changes of a game state" $ do
-            let d' = (Blue,[(0,2),(1,2)])
-            moves [a,b,c,d']  `shouldBe` 
-                [(Red,(0,0)),(Blue,(0,0))
-                ,(Brown,(0,1)),(Blue,(0,1))
-                ,(Red,(1,1)),(Blue,(1,1))
-                ,(Brown,(0,2)),(Red,(0,2))]
-
-    describe "solve" $ do 
-        it "solves the game for a given number of moves" $ do
-            (solve 1 [(Red,[(0,0)]),(Brown,[(1,0)])]) `shouldBe` [(0,(Brown,(0,0))),(0,(Red,(1,0)))] 
-            (solve 1 [(Red,[(0,0)]),(Brown,[(1,0)]),(Red,[(2,0)])]) `shouldBe` [(0,(Red,(1,0)))] 
-            (solve 1 [(Blue,[(0,0)]),(Brown,[(1,0)]),(Red,[(2,0)])]) `shouldBe` []
-            (solve 2 [(Blue,[(0,0)]),(Brown,[(1,0)]),(Red,[(2,0)])]) `shouldBe` 
-                []
-
-            
